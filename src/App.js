5448123d52d4c3d75dd33c4e258c894b0fabbff3
SoleMJS <mzubenko661@gmail.com>
2023-12-31 11:10:51 +0300

    feat:(src/App.js): Реализовал список дел чз jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-	return <div></div>
+	const [todos, setTodos] = useState([])
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then(response => response.json())
+			.then(json => setTodos(json))
+	}, [])
+
+	return (
+		<div className='container mt-4'>
+			<h1>Todo List</h1>
+			<ul className='list-group'>
+				{todos.map(todo => (
+					<li key={todo.id} className='list-group-item'>
+						{todo.title}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default App
