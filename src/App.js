aff8840d1a419f4ab6400633266fb65533c51f86
SoleMJS <mzubenko661@gmail.com>
2023-12-31 12:09:43 +0300

    feat:(src): Реализовал CRUD с помощью JSON-Server

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import {
+	useRequestAddTodo,
+	useRequestDeleteTodo,
+	useRequestGetTodo,
+	useRequestUpdateTodo,
+} from './hooks'
 
 function App() {
-	const [todos, setTodos] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
+	const [refreshTodosFlag, setRefreshTodosFlag] = useState(false)
 
-	useEffect(() => {
-		setIsLoading(true)
-		fetch('https://jsonplaceholder.typicode.com/todos')
-			.then(response => response.json())
-			.then(json => setTodos(json))
-		setIsLoading(false)
-	}, [])
+	const refreshTodos = () => setRefreshTodosFlag(!refreshTodosFlag)
+
+	const { isLoading, todos } = useRequestGetTodo(refreshTodosFlag)
+	const { isCreating, requestAddTodo } = useRequestAddTodo(refreshTodos)
+	const { isUpdating, requestUpdateTodo } = useRequestUpdateTodo(refreshTodos)
+	const { isDeleting, requestDeleteTodo } = useRequestDeleteTodo(refreshTodos)
 
 	return (
 		<div className='container mt-4'>
 			<h1>Todo List</h1>
-			<ul className='list-group'>
-				{isLoading ? (
-					<div className='loader'></div>
-				) : (
-					todos.map(({ id, title }) => (
-						<li key={id} className='list-group-item'>
-							{title}
-						</li>
-					))
-				)}
-			</ul>
+			{isLoading ? (
+				<div className='loader'></div>
+			) : (
+				<>
+					<ul className='list-group'>
+						{todos.map(({ id, title }) => (
+							<li key={id} className='list-group-item'>
+								{title}
+							</li>
+						))}
+					</ul>
+
+					<button
+						disabled={isCreating}
+						className='btn btn-primary m-2'
+						onClick={requestAddTodo}
+					>
+						Добавить задачу
+					</button>
+					<button
+						className='btn btn-primary m-2'
+						disabled={isUpdating}
+						onClick={requestUpdateTodo}
+					>
+						Обновить задачу
+					</button>
+					<button
+						className='btn btn-danger m-2'
+						disabled={isDeleting}
+						onClick={requestDeleteTodo}
+					>
+						Удалить задачу
+					</button>
+				</>
+			)}
 		</div>
 	)
 }
