5dc886d957d351b895c632c9bd44b9cd0be767bc
SoleMJS <mzubenko661@gmail.com>
2023-12-31 11:20:39 +0300

    feat:(src/App.js): Добавил loader

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import './App.css'
 
 function App() {
 	const [todos, setTodos] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
 
 	useEffect(() => {
+		setIsLoading(true)
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then(response => response.json())
 			.then(json => setTodos(json))
+		setIsLoading(false)
 	}, [])
 
 	return (
 		<div className='container mt-4'>
 			<h1>Todo List</h1>
 			<ul className='list-group'>
-				{todos.map(({ id, title }) => (
-					<li key={id} className='list-group-item'>
-						{title}
-					</li>
-				))}
+				{isLoading ? (
+					<div className='loader'></div>
+				) : (
+					todos.map(({ id, title }) => (
+						<li key={id} className='list-group-item'>
+							{title}
+						</li>
+					))
+				)}
 			</ul>
 		</div>
 	)
